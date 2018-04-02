b389f7798ac52886ba1d5fba24cb4df263a90033
Stanlous <ystanlous@gmail.com>
2018-04-02 15:13:00 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const hasha = require('hasha')
+
+module.exports = class ContentHash {
+  apply(compiler) {
+    
+    compiler.plugin('compilation', (compilation) => {
+      compilation.plugin(
+        'html-webpack-plugin-after-emit',
+        ({ outputName, html }) => {
+          const chunk = compilation.assets[outputName]
+          const hash = hasha(html.source(), { 
+            algorithm: 'md5' 
+          }).substring(0, 20)
+          const name = outputName.replace(/\[contenthash\]/g, hash)
+          compilation.assets[name] = chunk
+          delete compilation.assets[outputName]
+        }
+      )
+    })
+    
+  }
+}
+
+
+
+
+
+
